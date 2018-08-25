702626ce14c12576b7756683f21624abfecd9e69
Michael Frick <michaelfrick@thoughtworks.com>
2018-08-25 12:02:28 +0200

    creating project

---
+++ PicsLock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PicsLock
+//
+//  Created by Michael Frick on 25/08/2018.
+//  Copyright © 2018 Michael Frick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
