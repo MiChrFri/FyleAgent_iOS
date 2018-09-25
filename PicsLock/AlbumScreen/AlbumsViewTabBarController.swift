a746f959b557414d753f1c93db94481da60c7899
Michael Frick <mfrick@thoughtworks.com>
2018-09-25 20:25:17 +0200

    adding basic search

---
+++ PicsLock/AlbumScreen/AlbumsViewTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  AlbumsViewTabBarController.swift
+//  PicsLock
+//
+//  Created by Michael Frick on 25/09/2018.
+//  Copyright © 2018 Michael Frick. All rights reserved.
+//
+
+import Foundation
---
+++ PicsLock/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  PicsLock
+//
+//  Created by Michael Frick on 25/09/2018.
+//  Copyright © 2018 Michael Frick. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
